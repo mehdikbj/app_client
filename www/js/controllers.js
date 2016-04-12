fba6580adc8129a781b4687080c017b2e10d02e2
Mehdi Kabbaj <mehdikabbaj@octo-AEL.Home>
2016-04-12 11:45:48 +0100

    2eme commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,11 +4,46 @@ angular.module('starter.controllers', [])
 })
 
 .controller('agencesCtrl', function($scope, $http) {
+
+  $scope.map = { center: { latitude: 33.5910948, longitude: -7.6137281 }, zoom: 5 };
   $scope.agences=[];
-  $scope.mavariable="salut";
-  $http.get('/api/action/datastore_search?resource_id=9079d1fd-2425-4a31-9557-0b056146ff28')
-          .then(function(response){
-            console.log(response);
+
+var prefixApi ='';
+
+if(window.cordova){
+  prefixApi = 'http://demo.ckan.org';
+}
+
+  $http.get( prefixApi + '/api/action/datastore_search?resource_id=9079d1fd-2425-4a31-9557-0b056146ff28').then(function(response){
+            
             $scope.agences = response.data.result.records;
+            $scope.myMarkers = extractMarkersFromData(response.data.result.records);
           });
+
+
+  function extractMarkersFromData(data){
+    var allMarkers = [];
+
+    var marker = {
+      id:"2",
+      coord:{
+        longitude: "",
+        latitude:""
+      }
+    }
+
+    for(var i=0 ;i<data.length ;i++){
+      var marker = {
+        id: data[i]._id,
+        coord: {
+                  longitude: data[i].fLongitude,
+                  latitude: data[i].fLatitude
+                }
+         };
+
+      allMarkers.push(marker);  
+    }
+    return allMarkers;
+  }
+
 });
